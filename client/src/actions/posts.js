1efea1970053e3645ec30e235dfe36bc7c0b5f5e
preetam mondal <preetammondal78@gmail.com>
2022-11-13 16:11:59 +0530

    first commit done

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -7,7 +7,25 @@ export const getPosts =  () => async (dispatch) => {
         const {data} = await api.fetchPosts();
         dispatch({type:"FETCH_ALL", payload:data});         /* ==> action is an object with type and payload*/
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 
+}
+
+export const createPost = (post) => async(dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+        dispatch({type: "CREATE", payload:data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async(dispatch) => {
+    try {
+       const {data} =  await api.updatePost(id, post);
+       dispatch({type: "UPDATE", payload: data})
+    } catch ({error}) {
+        console.log(error);
+    }
 }
